1f1fd3f9440d03a848c033ce287f179f49d038a7
anonymous anonymous <sophiajhooper@gmail.com>
2021-11-15 19:38:18 +0000

    Indirect association between movies and actors

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,6 +15,10 @@ class Movie < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :cast,
+             :through => :roles,
+             :source => :actor
+
   has_many   :reviewers,
              :through => :reviews,
              :source => :reviewer
